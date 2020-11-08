44ccae85f66b0a86a0ff606cc8486ebb0d5097c3
Samara <41492804+samara-c@users.noreply.github.com>
2020-11-07 23:18:22 -0300

    Add files via upload

---
+++ js/selecionarmesa.js
@@ -0,0 +1,132 @@
+console.log("Estou aprendendo Javascript");
+window.onload = function (){
+    var mesas = [
+    {
+        nome : "Marte",
+        imagem : "./icons/marte.PNG",
+        descricao : "Sinta-se um habitante do planeta vermelho nessa mesa completamente customizada",
+        localizacao: "Térreo",
+        capacidade: 3
+
+
+    },
+
+    {
+        nome : "Plutao",
+        imagem : "./icons/marte.PNG",
+        descricao : "Sinta-se um habitante do planeta vermelho nessa mesa completamente customizada",
+        localizacao: "terreo",
+        capacidade: 4
+
+
+    },
+
+    {
+        nome : "Plutao",
+        imagem : "./icons/marte.PNG",
+        descricao : "Sinta-se um habitante do planeta vermelho nessa mesa completamente customizada",
+        localizacao: "terreo",
+        capacidade: 4
+
+
+    },
+
+    {
+        nome : "Plutao",
+        imagem : "./icons/marte.PNG",
+        descricao : "Sinta-se um habitante do planeta vermelho nessa mesa completamente customizada",
+        localizacao: "terreo",
+        capacidade: 4
+
+
+    },
+
+    {
+        nome : "Marte",
+        imagem : "./icons/marte.PNG",
+        descricao : "Sinta-se um habitante do planeta vermelho nessa mesa completamente customizada",
+        localizacao: "Térreo",
+        capacidade: 3
+
+
+    },
+
+    {
+        nome : "Plutao",
+        imagem : "./icons/marte.PNG",
+        descricao : "Sinta-se um habitante do planeta vermelho nessa mesa completamente customizada",
+        localizacao: "terreo",
+        capacidade: 4
+
+
+    },
+
+    {
+        nome : "Plutao",
+        imagem : "./icons/marte.PNG",
+        descricao : "Sinta-se um habitante do planeta vermelho nessa mesa completamente customizada",
+        localizacao: "terreo",
+        capacidade: 4
+
+
+    },
+
+    {
+        nome : "Plutao",
+        imagem : "./icons/marte.PNG",
+        descricao : "Sinta-se um habitante do planeta vermelho nessa mesa completamente customizada",
+        localizacao: "terreo",
+        capacidade: 4
+
+
+    },
+    {
+        nome : "Plutao",
+        imagem : "./icons/marte.PNG",
+        descricao : "Sinta-se um habitante do planeta vermelho nessa mesa completamente customizada",
+        localizacao: "terreo",
+        capacidade: 4
+
+
+    }
+]
+    adicionarMesas(mesas);
+}
+
+
+
+function adicionarMesas(mesas) {
+    console.log("passei por aqui");
+    let mesasDiv=document.getElementById("listaMesas");
+    for (var i = 0; i < mesas.length; i++) {
+
+    let cardMesas = document.createElement("div");
+    cardMesas.className="backgroundCardMesa";
+    mesasDiv.appendChild(cardMesas);
+    console.log({mesasDiv});
+    let articleMesas = document.createElement("article");
+    articleMesas.className="conteudoCardMesas";
+    let planetaNome = document.createElement("p");
+    let planetaImagem = document.createElement("img");
+    planetaImagem.className="imagemMesa";
+    let planetaDescricao = document.createElement("p");
+    let planetaLocalizacao = document.createElement("p");
+    let planetaCapacidade = document.createElement("p");
+    planetaNome.innerHTML ="<span class='tituloMesas'>"+mesas[i].nome+"</span>";
+    planetaImagem.src = mesas[i].imagem;
+    planetaDescricao.innerHTML = mesas[i].descricao;
+    planetaLocalizacao.innerHTML = "<span class='classeDoSpan'>Localização: </span>"+mesas[i].localizacao;
+    planetaCapacidade.innerHTML = "<span class='classeDoSpan'>Capacidade: </span>"+mesas[i].capacidade;
+    articleMesas.appendChild(planetaNome);
+    articleMesas.appendChild(planetaImagem);
+    articleMesas.appendChild(planetaDescricao);
+    articleMesas.appendChild(planetaLocalizacao);
+    articleMesas.appendChild(planetaCapacidade);
+    cardMesas.appendChild(articleMesas);
+    }
+
+}
+
+function carregarMesas () {
+    mesas.forEach (mesa => adicionarMesa(mesa.nome, mesa.imagem, mesa.descricao, mesa.localizacao, mesa.capacidade));
+}
